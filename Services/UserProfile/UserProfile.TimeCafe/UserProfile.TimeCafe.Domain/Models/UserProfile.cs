d2ee52adb6e688ba4702cd7909d325dae4beb237
Даниил Клименков <klimenkokov1@gmail.com>
2025-10-23 17:56:08 +0300

    features: добавить сервис для работы с профилями.

---
+++ Services/UserProfile/UserProfile.TimeCafe/UserProfile.TimeCafe.Domain/Models/UserProfile.cs
@@ -0,0 +1,21 @@
+﻿namespace UserProfile.TimeCafe.Domain.Models;
+
+public class UserProfile
+{
+    public string UserId { get; set; } 
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string? MiddleName { get; set; }
+    public Gender? Gender { get; set; }
+    public DateOnly? BirthDate { get; set; }
+    public string AccessCardNumber { get; set; }
+    public string? PhotoUrl { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
+
+public enum Gender : byte
+{
+    NotSpecified = 0,
+    Male = 1,
+    Female = 2
+}
