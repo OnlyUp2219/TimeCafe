8e71d755d378453a72c91501623cb5214a7604f0
Даниил Клименков <klimenkokov1@gmail.com>
2025-11-18 20:02:47 +0300

    features: добавить интеграционные тесты для управления пользователями и ролями

---
+++ Services/Auth/Auth.TimeCafe.Test/Integration/IntegrationTestFactory.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Auth.TimeCafe.Infrastructure.Data;
+using Auth.TimeCafe.Domain.Contracts;
+
+
+namespace Auth.TimeCafe.Test.Integration;
+
+public class IntegrationTestFactory
+{
+    public ServiceProvider Services { get; }
+
+    public IntegrationTestFactory()
+    {
+        var serviceCollection = new ServiceCollection();
+
+        serviceCollection.AddDbContext<ApplicationDbContext>(options =>
+            options.UseInMemoryDatabase("IntegrationTestDb"));
+
+        serviceCollection.AddIdentityCore<IdentityUser>()
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        serviceCollection.AddScoped<IUserRoleService, UserRoleService>();
+        serviceCollection.AddScoped<IPermissionService, PermissionService>();
+
+        Services = serviceCollection.BuildServiceProvider();
+    }
+}
