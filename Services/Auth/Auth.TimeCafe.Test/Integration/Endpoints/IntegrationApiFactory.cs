90450556c8bc197550b82c0f4082df4ba496ce45
Даниил Клименков <klimenkokov1@gmail.com>
2025-11-19 19:35:24 +0300

    features: добавить интеграционные тесты для аутентификации пользователей

---
+++ Services/Auth/Auth.TimeCafe.Test/Integration/Endpoints/IntegrationApiFactory.cs
@@ -0,0 +1,39 @@
+namespace Auth.TimeCafe.Test.Integration.Endpoints;
+
+public class IntegrationApiFactory : WebApplicationFactory<Program>
+{
+    private readonly string _dbName = $"IntegrationTestsDb_{Guid.NewGuid()}";
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Testing");
+
+        builder.ConfigureAppConfiguration((ctx, cfg) =>
+        {
+            var overrides = new Dictionary<string, string?>
+            {
+                ["Kestrel:Endpoints:Https:Certificate:Path"] = string.Empty,
+                ["Kestrel:Endpoints:Https:Certificate:Password"] = string.Empty,
+                ["ConnectionStrings:DefaultConnection"] = string.Empty,
+                ["Seed:Admin:Email"] = "test@admin.com",
+                ["Seed:Admin:Password"] = "TestP@ssw0rd!",
+                ["Seed:Admin:Role"] = "admin"
+            };
+            cfg.AddInMemoryCollection(overrides);
+        });
+
+        builder.ConfigureTestServices(services =>
+        {
+            var dbContextOptionsDescriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(IDbContextOptionsConfiguration<ApplicationDbContext>));
+
+            if (dbContextOptionsDescriptor != null)
+            {
+                services.Remove(dbContextOptionsDescriptor);
+            }
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase(_dbName));
+        });
+    }
+}
