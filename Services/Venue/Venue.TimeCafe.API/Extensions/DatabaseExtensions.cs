814f2e3b1fad14ffcc7eb0aaf8bcb0b7d7a7d98b
Даниил Клименков <klimenkokov1@gmail.com>
2025-12-02 19:58:35 +0300

    refactor: добавить и улучшить расширения для конфигурации CORS, базы данных и интеграции RabbitMQ, улучшить обработку миграций и Swagger

--- Services/Venue/Venue.TimeCafe.API/Extensions/DatabaseExtensions.cs
+++ Services/Venue/Venue.TimeCafe.API/Extensions/DatabaseExtensions.cs
@@ -4,8 +4,21 @@ public static class DatabaseExtensions
 {
     public static IServiceCollection AddVenueDatabase(this IServiceCollection services, IConfiguration configuration)
     {
+        var connectionString = configuration.GetConnectionString("DefaultConnection")
+            ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
         services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+            options.UseNpgsql(connectionString));
         return services;
+    }
+
+    public static async Task ApplyMigrationsAsync(this WebApplication app)
+    {
+        if (app.Configuration.GetValue<bool>("SkipMigrations"))
+            return;
+
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        await dbContext.Database.MigrateAsync();
     }
 }
--- Services/Venue/Venue.TimeCafe.API/Program.cs
+++ Services/Venue/Venue.TimeCafe.API/Program.cs
@@ -1,20 +1,43 @@
-
 var builder = WebApplication.CreateBuilder(args);
 
+// CORS
+var corsPolicyName = builder.Configuration["CORS:PolicyName"]
+    ?? throw new InvalidOperationException("CORS:PolicyName is not configured.");
+builder.Services.AddCorsConfiguration(corsPolicyName);
+
+// MassTransit (отключён для разработки без RabbitMQ)
+//builder.Services.AddRabbitMqMessaging(builder.Configuration);
+
+// DbContext
+builder.Services.AddVenueDatabase(builder.Configuration);
+
+// Repositories
+builder.Services.AddVenuePersistence();
+
+// CQRS (MediatR + Pipeline Behaviors)
+builder.Services.AddVenueCqrs();
+
+// Swagger & Carter
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApiConfiguration();
-builder.Services.AddCarterConfiguration();
-builder.Services.AddVenueCqrs();
-builder.Services.AddVenueDatabase(builder.Configuration);
+builder.Services.AddCarter();
 
 var app = builder.Build();
 
-app.UseScalarConfiguration();
+// app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+await app.ApplyMigrationsAsync();
+
+app.UseCors(corsPolicyName);
+
+app.UseSwaggerDevelopment();
 
 app.MapCarter();
 app.MapControllers();
 
-app.Run();
+await app.RunAsync();
+
+public partial class Program { }
 
 
