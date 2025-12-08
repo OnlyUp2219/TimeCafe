95eccfce8b3128a24de1c485e993e33e16141288
Даниил Клименков <klimenkokov1@gmail.com>
2025-12-08 18:15:11 +0300

    features: добавить миграцию для изменения типа идентификаторов на Guid

--- Services/UserProfile/UserProfile.TimeCafe.Infrastructure/Migrations/20251208151240_GuidMigration.Designer.cs
+++ Services/UserProfile/UserProfile.TimeCafe.Infrastructure/Migrations/20251208151240_GuidMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UserProfile.TimeCafe.Infrastructure.Data;
@@ -11,9 +12,11 @@ using UserProfile.TimeCafe.Infrastructure.Data;
 namespace UserProfile.TimeCafe.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251208151240_GuidMigration")]
+    partial class GuidMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
