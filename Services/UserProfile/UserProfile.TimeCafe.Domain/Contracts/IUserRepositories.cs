6eb46b9d048521194990af4d747e4e70a835e74c
Даниил Клименков <klimenkokov1@gmail.com>
2025-12-08 17:40:39 +0300

    features: обновить идентификацию пользователей для использования Guid вместо string в репозиториях и моделях, перенести DTO

--- Services/UserProfile/UserProfile.TimeCafe.Domain/Contracts/IUserRepositories.cs
+++ Services/UserProfile/UserProfile.TimeCafe.Domain/Contracts/IUserRepositories.cs
@@ -5,10 +5,10 @@ public interface IUserRepositories
     Task<IEnumerable<Profile?>> GetAllProfilesAsync(CancellationToken? cancellationToken);
     Task<IEnumerable<Profile?>> GetProfilesPageAsync(int pageNumber, int pageSize, CancellationToken? cancellationToken);
     Task<int> GetTotalPageAsync(CancellationToken? cancellationToken);
-    Task<Profile?> GetProfileByIdAsync(string userId, CancellationToken? cancellationToken);
+    Task<Profile?> GetProfileByIdAsync(Guid userId, CancellationToken? cancellationToken);
     Task<Profile?> CreateProfileAsync(Profile profile, CancellationToken? cancellationToken);
     Task<Profile?> UpdateProfileAsync(Profile profile, CancellationToken? cancellationToken);
-    Task DeleteProfileAsync(string userId, CancellationToken? cancellationToken);
-    Task CreateEmptyAsync(string userId, CancellationToken? cancellationToken);
+    Task DeleteProfileAsync(Guid userId, CancellationToken? cancellationToken);
+    Task CreateEmptyAsync(Guid userId, CancellationToken? cancellationToken);
 
 }
