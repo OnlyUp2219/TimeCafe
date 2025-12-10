a3b6b055a1222725ed01c52b3b35f97d6e5aa3c1
Даниил Клименков <klimenkokov1@gmail.com>
2025-12-10 12:46:40 +0300

    refactor: изменить идентификаторы на Guid в Promotion и связанных репозиториях

--- Services/Venue/Venue.TimeCafe.Domain/Models/Promotion.cs
+++ Services/Venue/Venue.TimeCafe.Domain/Models/Promotion.cs
@@ -2,7 +2,7 @@ namespace Venue.TimeCafe.Domain.Models;
 
 public class Promotion
 {
-    public int PromotionId { get; set; }
+    public Guid PromotionId { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public decimal? DiscountPercent { get; set; }
