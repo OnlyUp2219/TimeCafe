fbbc69dc9e8ef0cfb8595e2d7094c5acfbfac1c3
Даниил Клименков <klimenkokov1@gmail.com>
2025-12-28 14:46:47 +0300

    features: добавить интеграционные тесты для системы авторизации и управления разрешениями

--- Services/UserProfile/UserProfile.TimeCafe.Test/Integration/Helpers/TestData.cs
+++ Services/UserProfile/UserProfile.TimeCafe.Test/Integration/Helpers/TestData.cs
@@ -129,4 +129,19 @@ public static class TestData
         yield return (ExistingUsers.User2Id, ExistingUsers.User2FirstName, ExistingUsers.User2LastName, ExistingUsers.User2Gender);
         yield return (ExistingUsers.User3Id, ExistingUsers.User3FirstName, ExistingUsers.User3LastName, ExistingUsers.User3Gender);
     }
+
+    public static class Auth
+    {
+        public static readonly Guid DefaultUserId = Guid.Parse("11111111-1111-1111-1111-111111111111");
+        public static readonly Guid AdminUserId = Guid.Parse("22222222-2222-2222-2222-222222222222");
+        public static readonly Guid ClientUserId = Guid.Parse("33333333-3333-3333-3333-333333333333");
+        public static readonly Guid OtherUserId = Guid.Parse("44444444-4444-4444-4444-444444444444");
+
+        public const string AdminRole = "admin";
+        public const string ClientRole = "client";
+        public const string GuestRole = "guest";
+
+        public const int AdminPermissionCount = 8;
+        public const int ClientPermissionCount = 4;
+    }
 }
