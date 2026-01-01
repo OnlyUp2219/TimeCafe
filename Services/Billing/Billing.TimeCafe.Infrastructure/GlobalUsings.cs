4db214a81d16687a0539c9841d0c31aa90497dff
Даниил Клименков <klimenkokov1@gmail.com>
2026-01-01 17:00:29 +0300

    features: добавить репозитории в Billing.

--- Services/Billing/Billing.TimeCafe.Infrastructure/GlobalUsings.cs
+++ Services/Billing/Billing.TimeCafe.Infrastructure/GlobalUsings.cs
@@ -9,4 +9,6 @@ global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Logging;
 
 global using Billing.TimeCafe.Domain.Constants;
+global using Billing.TimeCafe.Domain.Models;
+global using Billing.TimeCafe.Domain.Repositories;
 global using Billing.TimeCafe.Infrastructure.Data;
