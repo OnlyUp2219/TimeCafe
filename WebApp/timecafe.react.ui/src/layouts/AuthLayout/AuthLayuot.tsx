366ef7958408fe1c89cc4c258e73c16c9eff2a36
Даниил Клименков <klimenkokov1@gmail.com>
2026-01-12 16:58:34 +0300

    refactor: упростить AuthLayout, обновить LoginPage, удалить старые страницы

--- WebApp/timecafe.react.ui/src/layouts/AuthLayout/AuthLayuot.tsx
+++ WebApp/timecafe.react.ui/src/layouts/AuthLayout/AuthLayuot.tsx
@@ -2,9 +2,5 @@ import {Outlet} from "react-router-dom";
 import type {FC} from "react";
 
 export const AuthLayout: FC = () => {
-    return (
-        <div className="auth-layout">
-            <main className="auth-layout__main"><Outlet/></main>
-        </div>
-    );
+    return <Outlet/>;
 };
