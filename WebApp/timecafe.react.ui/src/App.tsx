b7832c93f4c5a3d9a15676437663d5c3d19ef479
Даниил Клименков <klimenkokov1@gmail.com>
2026-01-12 19:42:19 +0300

    features: добавить страницы сброса пароля и подтверждения сброса в маршруты

--- WebApp/timecafe.react.ui/src/App.tsx
+++ WebApp/timecafe.react.ui/src/App.tsx
@@ -5,6 +5,8 @@ import './App.css';
 import "./api/interceptors.ts"
 import { LoginPage } from './pages/LoginPage';
 import { RegisterPage } from './pages/RegisterPage';
+import { ResetPasswordPage } from './pages/ResetPasswordPage';
+import { ConfirmResetPage } from './pages/ConfirmResetPage';
 
 
 const AuthLayout = lazy(() => import('./layouts/AuthLayout/AuthLayuot').then(module => ({default: module.AuthLayout})));
@@ -18,6 +20,8 @@ export default function App() {
                     <Route element={<AuthLayout/>}>
                         <Route path="/login" element={<LoginPage/>}/>
                         <Route path="/register" element={<RegisterPage/>}/>
+                        <Route path="/reset-password" element={<ResetPasswordPage/>}/>
+                        <Route path="/confirm-reset" element={<ConfirmResetPage/>}/>
 
                         <Route path="/" element={<Navigate to="/login" replace/>}/>
                     </Route>
