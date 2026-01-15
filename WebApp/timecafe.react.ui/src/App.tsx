c396d0a2fcfc1f3ed29a2fc3c303ccccd64753c9
Даниил Клименков <klimenkokov1@gmail.com>
2026-01-15 20:47:15 +0300

    features: отрефакторить стили и компоненты; добавить страницы для персональных данных

--- WebApp/timecafe.react.ui/src/App.tsx
+++ WebApp/timecafe.react.ui/src/App.tsx
@@ -1,18 +1,18 @@
 // App.tsx
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import {lazy} from 'react';
+
 import './App.css';
 import "./api/interceptors.ts"
-import { LoginPage } from './pages/LoginPage';
-import { RegisterPage } from './pages/RegisterPage';
-import { ResetPasswordPage } from './pages/ResetPasswordPage';
-import { ConfirmResetPage } from './pages/ConfirmResetPage';
-import { LandingPage } from './pages/LandingPage.tsx';
-import { HomePage } from './pages/HomePage';
+import {LoginPage} from './pages/auth/LoginPage';
+import {RegisterPage} from './pages/auth/RegisterPage';
+import {ResetPasswordPage} from './pages/auth/ResetPasswordPage';
+import {ConfirmResetPage} from './pages/auth/ConfirmResetPage';
+import {LandingPage} from './pages/public/LandingPage';
+import {HomePage} from './pages/home/HomePage';
+import {PersonalDataPage} from "./pages/profile/PersonalDataPage";
+import {AuthLayout} from './layouts/AuthLayout/AuthLayuot';
+import {MainLayout} from './layouts/MainLayout/MainLayuot';
 
-
-const AuthLayout = lazy(() => import('./layouts/AuthLayout/AuthLayuot').then(module => ({default: module.AuthLayout})));
-const MainLayout = lazy(() => import('./layouts/MainLayout/MainLayuot').then(module => ({default: module.MainLayout})));
 
 export default function App() {
     return (
@@ -29,7 +29,10 @@ export default function App() {
                     </Route>
 
                     <Route element={<MainLayout/>}>
-                        <Route path="/home" element={<HomePage/>} />
+                        <Route path="/home" element={<HomePage/>}/>
+                        <Route path="/personal-data" element={<PersonalDataPage/>}/>
+                        <Route path="/login-test" element={<LoginPage/>}/>
+
                     </Route>
 
                 </Routes>
