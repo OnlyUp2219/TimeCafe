9c667740a170ad5a52e9aded181758482906c9d7
Даниил Клименков <klimenkokov1@gmail.com>
2026-01-24 16:06:30 +0300

    refactor: удалить устаревшие классы и интерфейсы для обработки результатов CQRS и аутентификации

--- BuildingBlocks/Extensions/ICqrsResult.cs
+++
@@ -1,9 +0,0 @@
-namespace BuildingBlocks.Extensions;
-
-public interface ICqrsResult
-{
-    bool Success { get; }
-    string? Message { get; }
-    List<string>? Errors { get; }
-    ETypeError? TypeError { get; }
-}
